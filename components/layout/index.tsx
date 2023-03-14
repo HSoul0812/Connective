ba1e28e689298ac4a47f0788f69fb65627d5773b
HSoul0812 <hugeouls0813@gmail.com>
2023-03-14 05:04:20 -0700

    initial commit

---
+++ components/layout/index.tsx
@@ -0,0 +1,36 @@
+import { User } from '../../types/types'
+import Sidebar from '../sidebar'
+
+type Props = {
+  title: string
+  scroll?: boolean
+  user: User
+  children: React.ReactNode
+}
+
+const Layout = ({ title, scroll = true, user, children }: Props) => {
+  return (
+    <main
+      className={`flex flex-row ${
+        scroll ? 'min-h-screen' : 'h-screen max-h-screen'
+      } min-w-screen font-[Montserrat]`}
+    >
+      <Sidebar user={user}></Sidebar>
+      <div
+        className={`w-screen h-screen ${
+          scroll ? 'overflow-y-scroll' : 'h-full max-h-screen'
+        } flex flex-col relative`}
+      >
+        <p className="mt-[22px] ml-[40px] mb-[50px] text-[#A0AEC0] text-sm">
+          General /{' '}
+          <span className="font-bold text-3xl leading-[29px] text-[#0D1011]">
+            {title}
+          </span>
+        </p>
+        {children}
+      </div>
+    </main>
+  )
+}
+
+export default Layout
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      '1bp': { min: '2074px' },
+      '2bp': { max: '1241px' },
+      '3bp': { min: '2300px' },
+      '4bp': { max: '1905px' },
+      '5bp': { min: '1516px' },
+    },
+    extend: {
+      colors: {
+        white: '#FFFF',
+        'desaturated-cyan': '#5FB4A2',
+        'bright-red': '#F43030',
+        purple: '#7E38B7',
+        black: '#111',
+        gray: '#A0AEC0',
+        'light-gray': '#F8F9FA',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+}
